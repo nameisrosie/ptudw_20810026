57c0c09ded570a6077e9488b4025aaca9129b922
larose <133701479+nameisrosie@users.noreply.github.com>
2025-11-26 14:25:57 +0700

    fix catagories

--- client/src/components/SidebarCategories.jsx
+++ client/src/components/SidebarCategories.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom"; // <--- 1. Import Link
 import { http } from "../lib/http.js";
 
 export default function SidebarCategories() {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState();
   const [error, setError] = useState();
+
   useEffect(() => {
     let canceled = false;
 
@@ -15,7 +17,7 @@ export default function SidebarCategories() {
       try {
         const res = await http.get("/categories");
         const data = res.data.data;
-        console.log(res.data);
+        // console.log(res.data);
         if (!canceled) {
           setCategories(data);
           setLoading(false);
@@ -47,10 +49,11 @@ export default function SidebarCategories() {
           !error &&
           categories.map((c) => (
             <li className="main-nav-list" key={c.id}>
-              <a href={"/products?category=" + c.id}>
+              {/* 2. Sửa thẻ a thành Link để không bị reload trang */}
+              <Link to={"/products?category=" + c.id}>
                 {c.name}
-                <span className="number">({c.Products.length})</span>
-              </a>
+                <span className="number">({c.Products?.length || 0})</span>
+              </Link>
             </li>
           ))}
       </ul>
