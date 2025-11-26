4189a969ab7cb7e681be7a04ef0ec773c039f4f5
larose <133701479+nameisrosie@users.noreply.github.com>
2025-11-26 14:08:37 +0700

    fix http.js

--- client/src/lib/http.js
+++ client/src/lib/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const http = axios.create({
-  baseURL: "/api/v1", 
+  baseURL: import.meta.env.VITE_API_URL, 
   
   headers: {
     "Content-Type": "application/json",
